486f5b41245ecca6d804e03709219090ca3ac2f4
Unknown <daniahyasin95@gmail.com>
2017-10-21 16:48:57 +0800

    daniah the kraken

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -21,4 +21,6 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
+
+    abcdefghijklmnopqrstuvwxyz
 });
